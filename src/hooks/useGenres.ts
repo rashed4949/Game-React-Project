05ff184f2f58c5e48f9aadd3e4dc8ea2b11494b2
rashed4949 <rrashed4@gmail.com>
2024-10-02 08:49:55 +0200

    Refactor: create lookup hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
+import genres from "../data/genres";
 
 const apiClient = new APIClient<Genre>("/genres");
 export interface Genre {
@@ -13,6 +14,7 @@ const useGenres = () => {
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
+    initialData: genres,
   });
 };
 export default useGenres;
